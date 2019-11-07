6490779e59b8c1262cd2b25d01f949e394e351cf
Khoa Le <khoa.le@tripi.vn>
2019-11-07 22:27:53 +0700

    initialize project

---
+++ src/main/java/common/utils/DateFormat.java
@@ -0,0 +1,7 @@
+package common.utils;
+
+import org.apache.commons.lang3.time.FastDateFormat;
+
+public class DateFormat {
+    public static FastDateFormat yyyyMMddHHmmss_Hyphen = FastDateFormat.getInstance("yyyy-MM-dd HH-mm-ss");
+}
